7d1bfdb52b9dfb972f07f79deb6b05436a17f6aa
Roy Geagea <roygeagea@gmail.com>
2020-07-30 19:46:48 +0300

    Initial Commit

---
+++ rxswift-mvvmTests/rxswift_mvvmTests.swift
@@ -0,0 +1,34 @@
+//
+//  rxswift_mvvmTests.swift
+//  rxswift-mvvmTests
+//
+//  Created by Roy Geagea on 7/30/20.
+//  Copyright © 2020 Roy Geagea. All rights reserved.
+//
+
+import XCTest
+@testable import rxswift_mvvm
+
+class rxswift_mvvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
